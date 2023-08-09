4ba2a967ddad49ae82e13a2a0b8c9727acc8e12b
Filipe Bicho <fb@refusion.com>
2023-08-09 08:41:12 +0200

    create buttons

--- app/src/main/java/com/example/poker/GameDesign.kt
+++ app/src/main/java/com/example/poker/GameDesign.kt
@@ -17,12 +17,22 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -33,7 +43,10 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import androidx.core.graphics.toColorInt
+import kotlin.math.roundToInt
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Layout(game: Game) {
     Background()
@@ -158,8 +171,27 @@ fun Layout(game: Game) {
                     modifier = Modifier
                         .fillMaxHeight()
                         .weight(0.4f)
-                        .border(1.dp, Color.White)
+                        .border(1.dp, Color.White),
                 ) {
+                    Column {
+                        var sliderPosition by remember { mutableStateOf(0f) }
+                        Text(
+                            text = sliderPosition.roundToInt().toString()
+                        )
+                        Slider(
+                            value = sliderPosition,
+                            valueRange = 0f..1500f,
+                            onValueChange = { sliderPosition = it },
+
+                        )
+                        Row (
+                            horizontalArrangement = Arrangement.spacedBy(4.dp)
+                        ){
+                            Button("Fold")
+                            Button("Call")
+                            Button("Bet")
+                        }
+                    }
 
                 }
             }
@@ -251,4 +283,26 @@ private fun DealerChipImage() {
         contentDescription = "Dealer chip image",
         modifier = Modifier.size(20.dp)
     )
+}
+
+@Composable
+private fun Button(text: String) {
+    val contextForToast = LocalContext.current.applicationContext
+
+    Button(
+        modifier = Modifier
+            .width(80.dp)
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color("#a82020".toColorInt()),
+                        Color.Black
+                    ),
+                ), shape = ButtonDefaults.elevatedShape
+            ),
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+    ) {
+        Text(text, color = Color.White)
+    }
 }
