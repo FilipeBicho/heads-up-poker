36d399a5085055dae705fd7bba2b018a928a2d0f
Filipe Bicho <fb@refusion.com>
2025-07-17 18:18:18 +0200

    start intro improvement

--- app/src/main/java/com/filipebicho/pokerclash/ui/StartGameScreen.kt
+++ app/src/main/java/com/filipebicho/pokerclash/ui/StartGameScreen.kt
@@ -2,7 +2,6 @@ package com.filipebicho.pokerclash.ui
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateTo
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
@@ -12,35 +11,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.filipebicho.pokerclash.R
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
@@ -48,14 +39,16 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.offset
-import kotlin.math.roundToInt
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.filipebicho.pokerclash.R
 
 @Composable
 fun StartGameScreen(onStartButtonClicked: (String) -> Unit) {
@@ -70,21 +63,14 @@ fun StartGameScreen(onStartButtonClicked: (String) -> Unit) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 60.dp, start = 16.dp, end = 16.dp)
+                    .align(Alignment.Center)
+                    .padding(bottom = 100.dp, start = 16.dp, end = 16.dp)
             ) {
+
                 AnimatedPokerTitle()
                 Spacer(modifier = Modifier.height(24.dp))
-            }
-
-            // Bottom section: Input and Button
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Bottom,
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 100.dp, start = 32.dp, end = 32.dp)
-            ) {
+                Logo()
+                Spacer(modifier = Modifier.height(24.dp))
                 PlayerNameTextField(playerName = playerName, onPlayerNameChanged = { playerName = it })
                 Spacer(modifier = Modifier.height(24.dp))
                 if (playerName.isNotEmpty()) {
@@ -98,42 +84,45 @@ fun StartGameScreen(onStartButtonClicked: (String) -> Unit) {
 }
 
 @Composable
-fun AnimatedPokerTitle(modifier: Modifier = Modifier) { // Added modifier for flexibility, though not used in current StartGameScreen directly
-    val alpha = remember { Animatable(0f) }
-    val scale = remember { Animatable(0.5f) }
+fun Logo()
+{
+    Image(
+        painter = painterResource(R.drawable.logo),
+        contentScale = ContentScale.Fit,
+        contentDescription = "logo",
+        modifier = Modifier
+            .width(150.dp)
+            .height(150.dp)
+            .alpha(0.4f)
+    )
+}
 
-    LaunchedEffect(Unit) {
-        alpha.animateTo(
-            targetValue = 1f,
-            animationSpec = tween(durationMillis = 800, delayMillis = 0)
-        )
-        scale.animateTo(
-            targetValue = 1f,
-            animationSpec = spring(
-                dampingRatio = Spring.DampingRatioHighBouncy,
-                stiffness = Spring.StiffnessMediumLow,
+@Composable
+fun AnimatedPokerTitle(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row {
+            Text(
+                text = "AI",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 100.sp,
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.ExtraBold,
+                ),
             )
+        }
+
+        Text(
+            text = "Poker Clash",
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 50.sp,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.ExtraBold,
+            ),
         )
     }
 
-    Text(
-        text = "AI Poker Clash",
-        style = TextStyle(
-            color =colorResource(id = R.color.poker_red),
-            fontSize = 44.sp,
-            fontFamily = FontFamily.SansSerif,
-            fontWeight = FontWeight.ExtraBold,
-            shadow = Shadow(
-                color = Color.White.copy(alpha = 0.5f),
-                offset = Offset(3.0f, 3.0f),
-                blurRadius = 5f
-            ),
-            textAlign = TextAlign.Center
-        ),
-        modifier = modifier
-            .scale(scale.value)
-            .alpha(alpha.value)
-    )
 }
 
 @Composable
@@ -208,9 +197,7 @@ fun StartGameBackground(modifier: Modifier = Modifier, content: @Composable () -
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.Black.copy(alpha = 0.8f)) // Adjust alpha for desired dimness
-            // 0.0f = fully transparent, 1.0f = fully opaque black
-            // 0.5f to 0.7f is often a good range
+                .background(Color.Black.copy(alpha = 0.9f))
         )
         // The actual screen content is placed on top of the image
         content()
